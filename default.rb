c2f40d96a772669ad6b33201f25c9439aa90746b
Taro Aoki <aizu.s1230022@gmail.com>
2017-01-25 22:31:33 +0900

    [ADD] cask apps

--- default.rb
+++ default.rb
@@ -1,6 +1,18 @@
 require "yaml"
 
-conf = YAML.load_file("./brew.yml")
-conf["packages"].each do |pkg|
+BREW_INSTALL = "/usr/bin/ruby -e $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+conf = YAML.load_file("./packages.yml")
+
+execute BREW_INSTALL do
+  not_if "which brew > /dev/null"
+end
+
+conf["brew"].each do |pkg|
   package pkg
+end
+
+ENV["HOMEBREW_CASK_OPTS"] = "--appdir=/Applications"
+conf["cask"].each do |pkg|
+  execute "brew cask install #{pkg}"
 end
