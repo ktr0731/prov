cdfad70e04a7a9d5d246e4b19351c1fdb8087edf
lycoris0731 <aizu.s1230022@gmail.com>
2017-03-28 18:32:03 +0900

    [ADD] Java

--- default.rb
+++ default.rb
@@ -9,11 +9,12 @@ execute "Install Homebrew" do
   not_if "which brew > /dev/null"
 end
 
-conf["brew"].each do |pkg|
-  package pkg
-end
-
 ENV["HOMEBREW_CASK_OPTS"] = "--appdir=/Applications"
 conf["cask"].each do |pkg|
   execute "brew cask install #{pkg}"
+end
+
+# TODO: Java <- sbt の依存関係を解消したい
+conf["brew"].each do |pkg|
+  package pkg
 end
